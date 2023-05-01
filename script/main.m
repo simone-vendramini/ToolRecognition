a6bec1f43d63958cd9c501cb9b6dd8fd7f6d3c42
Simone Vendramini <s.vendramini1@campus.unimib.it>
2023-05-01 14:50:36 +0200

    Fix minimumBB

--- script/main.m
+++ script/main.m
@@ -20,32 +20,32 @@ se = strel('disk', 7);
 
 bw_m = imclose(bw, se);
 
-bw_m = filter_label(bw_m, 1000);
-
-labels = bwlabel(bw_m);
+%bw_m = filter_label(bw_m, 1000);
 
 %da modificare minimum_bounding box
 min_bbox = minimum_bounding_box(bw_m);
-proj = projections(min_bbox);
-
-figure(1);
-subplot(1, 3, 1), imshow(im);
-subplot(1, 3, 2), imshow(bw_m);
-subplot(1, 3, 3), imshow(min_bbox), axis equal;
-props = regionprops(min_bbox, 'all');
-
-figure(2);
-subplot(1, 2, 1), plot(proj.x);
-subplot(1, 2, 2), plot(proj.y);
-
-
-x = proj.x ./ max(proj.x);
-y = proj.y ./ max(proj.y);
-
-% Calcolo stats
-std_x = std(x);
-std_y = std(y);
-
-mean_x = mean(x);
-mean_y = mean(y);
+for i = 1: size(min_bbox)
+    proj = projections(min_bbox{i});
+    
+    figure();
+    subplot(1, 3, 1), imshow(im);
+    subplot(1, 3, 2), imshow(bw_m);
+    subplot(1, 3, 3), imshow(min_bbox{i}), axis equal;
+    props = regionprops(min_bbox{i}, 'all');
+    
+    figure();
+    subplot(1, 2, 1), plot(proj.x);
+    subplot(1, 2, 2), plot(proj.y);
+    
+    
+    x = proj.x ./ max(proj.x);
+    y = proj.y ./ max(proj.y);
+    
+    % Calcolo stats
+    std_x = std(x);
+    std_y = std(y);
+    
+    mean_x = mean(x);
+    mean_y = mean(y);
+end
 
--- script/minimum_bounding_box.m
+++ script/minimum_bounding_box.m
@@ -1,15 +1,24 @@
 %regionprops va effettuata su labels e non sull'intera immagine bw
 %l'out diventerà una lista di immagini bw dei singoli oggetti nella scena
 
-function out = minimum_bounding_box(image)
-    orStruct = regionprops(image,'Orientation');
-    or = orStruct.Orientation;
-    im_oriented = imrotate(image, 180-or);
+function out = minimum_bounding_box(bw)
+
+    labels = bwlabel(bw);
+
+    max_label = max(max(labels));
+    elements = cell(max_label,1);
     
-    st=regionprops(im_oriented,'all');
-    points = st.BoundingBox;
+    for n = 1 : max_label
+        element_n = (labels==n);
 
-    element = imcrop(im_oriented, [points(1), points(2), points(3), points(4)]);
-
-    out = element;
+        orStruct = regionprops(element_n,'Orientation');
+        or = orStruct.Orientation;
+        im_oriented = imrotate(element_n, 180-or);
+        st=regionprops(im_oriented,'all');
+        points = st.BoundingBox;
+        element = imcrop(im_oriented, [points(1), points(2), points(3), points(4)]);
+        elements{n} = element;
+        
+    end
+       out = elements;
 end

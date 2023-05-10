d471af2d6d49ce99100b6dfe89ad84b74ec36896
Simone Vendramini <102531922+Svendra4UniMiB@users.noreply.github.com>
2023-05-10 10:27:01 +0200

    Segmentazione con ROI v2.1

--- scripts/choose_segmentation.m
+++ scripts/choose_segmentation.m
@@ -2,14 +2,38 @@ function out = choose_segmentation(hist_hue, hist_sat)
     hist_hue = normalize_hist(hist_hue);
     hist_sat = normalize_hist(hist_sat);
 
-    pks_hue = findpeaks(hist_hue, 'MinPeakDistance', 20, 'MinPeakHeight', 0.05);
-    pks_sat = findpeaks(hist_sat, 'MinPeakDistance', 20, 'MinPeakHeight', 0.05);
+    pks_hue = findpeaks(hist_hue, 'MinPeakDistance', 20, 'MinPeakHeight', 0.03);
+    pks_sat = findpeaks(hist_sat, 'MinPeakDistance', 20, 'MinPeakHeight', 0.03);
 
-    out = [pks_sat, pks_hue];
+    max_pk_hue = 0;
+    max_pk_sat = 0;
+
+    for i = 1: numel(pks_hue)
+        if pks_hue(i) ~= 1
+            if pks_hue(i) > max_pk_hue
+                max_pk_hue = pks_hue(i);
+            end
+        end
+    end
+
+    for i = 1: numel(pks_sat)
+        if pks_sat(i) ~= 1
+            if pks_sat(i) > max_pk_sat
+                max_pk_sat = pks_sat(i);
+            end
+        end
+    end
+
+    if max_pk_sat > max_pk_hue
+        out = 1;
+    elseif  max_pk_hue > max_pk_sat
+        out = 2;
+    else
+        out = -1;
+    end
 end
 
 function out = normalize_hist(hist)
     max_value = max(hist);
-
     out = hist ./ max_value;
 end
--- scripts/main.m
+++ scripts/main.m
@@ -1,7 +1,7 @@
 close all;
 clear;
 
-im = imresize(imread("../dataset_v2/079.JPG"), 0.5);
+im = imresize(imread("../dataset_v2/075.JPG"), 0.5);
 
 im_hsv = rgb2hsv(im);
 
@@ -19,15 +19,28 @@ for i = 1:numel(objs)
     hist_sat = imhist(sat);
 
     code = choose_segmentation(hist_hue, hist_sat);
-    % Scegliamo la segmentazione in base al codice
-%     T = graythresh(objs{i}(:, :, 2));
-% 
-%     bw = imbinarize(objs{i}(:, :, 2), T);
+
+    figure();
+    subplot(1, 3, 1), imshow(im);
+
+    if code == 1
+        subplot(1, 3, 2), imshow(sat), title("saturation");
+        T = graythresh(sat);
+        bw2 = 1 - imbinarize(sat, T);
+    elseif code == 2
+        subplot(1, 3, 2), imshow(hue), title("hue");
+        T = graythresh(hue);
+        bw2 = 1 - imbinarize(hue, T);
+    else
+        %caso in cui non vanno bene entrambi
+    end
+    
+    subplot(1, 3, 3), imshow(bw2);
 end
 
-figure();
-subplot(1, 2, 1), imshow(im_hsv(:, :, 1));
-subplot(1, 2, 2), imshow(bw);
+% figure();
+% subplot(1, 2, 1), imshow(im_hsv(:, :, 1));
+% subplot(1, 2, 2), imshow(bw);
 
 
 

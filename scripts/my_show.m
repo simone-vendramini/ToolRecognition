13141b162fa6f67fc7738674f1ce046643daad43
Simone Vendramini <102531922+Svendra4UniMiB@users.noreply.github.com>
2023-05-10 10:50:32 +0200

    Aggiornamento my_show

--- scripts/my_show.m
+++ scripts/my_show.m
@@ -1,26 +1,14 @@
-close all;
-clear;
+function my_show(im_rgb)
 
-im_rgb = imread('../dataset_v2/079.JPG');
-% im_rgb = imread('../pinza.jpg');
-im = rgb2hsv(im_rgb);
-figure();
-subplot(1, 3, 1), imshow(im(:, :, 1));
-subplot(1, 3, 2), imshow(im(:, :, 2));
-subplot(1, 3, 3), imshow(im_rgb);
+    im = rgb2hsv(im_rgb);
 
-hist_sat = imhist(im(:, :, 2));
-mean_sat = mean(hist_sat);
-std_sat = std(hist_sat);
+    figure();
+    subplot(2, 3, 1), imshow(im_rgb);
+    subplot(2, 3, 2), imshow(im(:, :, 1)), title("Hue");
+    subplot(2, 3, 3), imshow(im(:, :, 2)), title("Saturation");
+    %subplot(2, 3, 4), plot(imhist(im(:, :, 1)));
+    subplot(2, 3, 5), plot(imhist(im(:, :, 1)));
+    subplot(2, 3, 6), plot(imhist(im(:, :, 2)));
+    
+end
 
-figure();
-subplot(1, 2, 1), plot(imhist(im(:, :, 1)));
-subplot(1, 2, 2), plot(imhist(im(:, :, 2)));
-
-line([0 255], [mean_sat mean_sat], 'LineWidth', 1, 'Color', 'r');
-
-T = graythresh(im(:,:, 2));
-
-bw = imbinarize(im(:, :, 2), T);
-
-figure(), imshow(1 - bw);

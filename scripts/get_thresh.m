51e923765ae5fdb9733e494ff7ab63b536c37718
Tommaso Ferrario <t.ferrario@campus.unimib.it>
2023-05-18 15:11:26 +0200

    :recycle: utilizzo dei metodi di matlab

--- scripts/get_thresh.m
+++ scripts/get_thresh.m
@@ -1,63 +1,89 @@
 function out = get_thresh(image)
 
     hist = imhist(image);
-
     hist = cat(1,hist,hist);
 
     max_index = find(hist==max(hist));
-    TF = islocalmin(hist);
+    local_min = islocalmin(hist);
     
-    T1 = 0;
-    T2 = 0;
+%     T1 = 0;
+%     T2 = 0;
+%     flag = 0;
 
-    flag = 0;
+    local_min_pos = find(local_min == 1);
 
     if max_index(1) > 128
+        diff = abs(local_min_pos - max_index(1));
         
-        for i = max_index(1) : -1 : 1
-            if TF(i) == 1
-                if flag
-                    flag = 0;
-                    T1 = i;
-                    break
-                else
-                    flag = 1;
-                end
-            end
+        index_closest = find(diff == min(diff));
+        if local_min_pos(index_closest) <= max_index(1)
+            T1 = local_min_pos(index_closest - 1);
+            T2 = local_min_pos(index_closest + 2);
+        else 
+            T1 = local_min_pos(index_closest - 2);
+            T2 = local_min_pos(index_closest + 1);
         end
         
-        for i = max_index(1) : 512
-             if TF(i) == 1
-                if flag
-                    T2 = i;
-                    break
-                else
-                    flag = 1;
-                end
-            end
-        end
+%         for i = max_index(1) : -1 : 1
+%             if TF(i) == 1
+%                 if flag
+%                     flag = 0;
+%                     T1 = i;
+%                     break
+%                 else
+%                     flag = 1;
+%                 end
+%             end
+%         end
+%         
+%         for i = max_index(1) : 512
+%              if TF(i) == 1
+%                 if flag
+%                     T2 = i;
+%                     break
+%                 else
+%                     flag = 1;
+%                 end
+%             end
+%         end
     else
-        for i = max_index(2) : -1 : 1
-             if TF(i) == 1
-                if flag
-                    flag = 0;
-                    T1 = i - 256;
-                    break
-                else
-                    flag = 1;
-                end
-            end
+        diff = abs(local_min_pos - max_index(2));
+        
+        index_closest = find(diff == min(diff));
+        if local_min_pos(index_closest) <= max_index(2)
+            T1 = local_min_pos(index_closest - 1);
+            T2 = local_min_pos(index_closest + 2);
+        else 
+            T1 = local_min_pos(index_closest - 2);
+            T2 = local_min_pos(index_closest + 1);
         end
-        for i = max_index(2) : 512
-            if TF(i) == 1
-                if flag
-                    T2 = i - 256;
-                    break
-                else
-                    flag = 1;
-                end
-            end
+        if T1 > 256
+            T1 = T1 - 256;
         end
+        if T2 > 256
+            T2 = T2 - 256;
+        end
+%         for i = max_index(2) : -1 : 1
+%              if TF(i) == 1
+%                 if flag
+%                     flag = 0;
+%                     T1 = i - 256;
+%                     break
+%                 else
+%                     flag = 1;
+%                 end
+%             end
+%         end
+%         for i = max_index(2) : 512
+%             if TF(i) == 1
+%                 if flag
+%                     T2 = i - 256;
+%                     break
+%                 else
+%                     flag = 1;
+%                 end
+%             end
+%         end
     end
 
 

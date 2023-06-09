72a9cd4f4f4c307cfb43b261b2815990bbfbd6c0
DavideBorzi <d.borzi1@campus.unimib.it>
2023-06-09 23:34:15 +0200

    formula sigma, nomi finestre train test

--- scripts/im_preprocess.m
+++ scripts/im_preprocess.m
@@ -1,6 +1,8 @@
 function out = im_preprocess(path)
 im = im2double(imresize(imread(path), 0.3));
-gaussFilt = fspecial("gaussian", [25 25], 2);
+n = 15;
+sigma = ceil((n-1)/5);
+gaussFilt = fspecial("gaussian", [n n], sigma);
 imG = im.^(0.7);
 out = imfilter(imG, gaussFilt);
 end
--- scripts/multi_classifier.m
+++ scripts/multi_classifier.m
@@ -21,26 +21,26 @@ test_tree = train_test_split.test;
 % load("train_test.mat");
 % KNN (15, mahalanobis)
 knnMahalanobis = fitcknn(train_knn.features, train_knn.labels, 'NumNeighbors',15, 'Distance','mahalanobis');
-acc_knn = model_evaluation(knnMahalanobis, train_knn, test_knn);
+acc_knn = model_evaluation(knnMahalanobis, train_knn, test_knn, "KNN");
 
 % Tree
 cart = fitctree(train_tree.features, train_tree.labels, "MaxNumSplits", 20);
 view(cart, "Mode","graph");
-acc_tree = model_evaluation(cart, train_tree, test_tree);
+acc_tree = model_evaluation(cart, train_tree, test_tree, "Tree");
 
 save("multi_classifier.mat", "knnMahalanobis", "cart");
 
-function out = model_evaluation(classifier, train, test)
+function out = model_evaluation(classifier, train, test, name)
     predict_train = predict(classifier, train.features);
     performance_train = confmat(predict_train, train.labels);
     
     predict_test = predict(classifier, test.features);
     performance_test = confmat(predict_test, test.labels);
     
-    figure();
+    figure("Name", "Train " + name);
     show_confmat(performance_train.cm_raw, performance_train.labels);
     
-    figure();
+    figure("Name", "Test " + name);
     show_confmat(performance_test.cm_raw, performance_test.labels);
     
     out = [performance_train.accuracy performance_test.accuracy];
